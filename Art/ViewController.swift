91247e7d5711840d7d0106a0f76926a39bba92b2
Kevin Stradtman <webkit.development.systems@gmail.com>
2022-04-07 00:46:05 -0400

    Initial Commit

---
+++ Art/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Art
+//
+//  Created by Kevin Stradtman on 4/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
